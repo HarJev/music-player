59d9647ecbc47ab79efb5927fc2843a13b062875
Jevaun Harris <jevaun.harris@smsja.net>
2019-09-10 15:07:46 -0500

    Further building out

--- src/components/common/Sidebar/View.js
+++ src/components/common/Sidebar/View.js
@@ -3,52 +3,116 @@ import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
 import RadioOutlinedIcon from '@material-ui/icons/RadioOutlined';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import HomeIcon from '@material-ui/icons/Home';
+import AddIcon from '@material-ui/icons/Add';
+import AlbumIcon from '../../../assets/album.png';
+import MicIcon from '../../../assets/microphone.png';
+import PlaylistIcon from '../../../assets/playlist.png';
+import PlaceIcon from '../../../assets/place.png';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
-import './View.css';
+import './style.css';
 
 export const SidebarView = () => (
   <div className="sb_box">
-    <div className="sb_circle_navs">
-      <div className="sb_circle_navs_item">
-        <div className="sb_circle_navs_circle">
-          <ExploreOutlinedIcon />
+    <PerfectScrollbar>
+      <div className="sb_circle_navs">
+        <div className="sb_circle_navs_item">
+          <div className="sb_circle_navs_circle">
+            <ExploreOutlinedIcon />
+          </div>
+          <div className="sb_circle_navs_text">Browse</div>
         </div>
-        <div className="sb_circle_navs_text">Browse</div>
-      </div>
-      <div className="sb_circle_navs_item">
-        <div className="sb_circle_navs_circle">
-          <RadioOutlinedIcon />
+        <div className="sb_circle_navs_item">
+          <div className="sb_circle_navs_circle">
+            <RadioOutlinedIcon />
+          </div>
+          <div className="sb_circle_navs_text">Radio</div>
         </div>
-        <div className="sb_circle_navs_text">Radio</div>
-      </div>
-      <div className="sb_circle_navs_item">
-        <div className="sb_circle_navs_circle">
-          <FavoriteBorderIcon />
+        <div className="sb_circle_navs_item">
+          <div className="sb_circle_navs_circle">
+            <FavoriteBorderIcon />
+          </div>
+          <div className="sb_circle_navs_text">Liked</div>
         </div>
-        <div className="sb_circle_navs_text">Liked</div>
       </div>
-    </div>
 
-    <div className="sb_madeForYou">
-      <div className="sb_madeForYou_btn">
-        <span className="sb_madeForYou_text">Made For You</span>
+      <div className="sb_madeForYou">
+        <div className="sb_madeForYou_btn">
+          <span className="sb_madeForYou_text">Made For You</span>
+        </div>
       </div>
-    </div>
 
-    <div className="sb_quickAccess">
-      <div className="sb_quickAccess_text">Quick Access</div>
-      <div className="sb_quickAccess_navs">
-        <div className="sb_quickAccess_nav">
-          <div className="sb_quickAccess_nav_indicator"></div>
-          <HomeIcon className="sb_quickAccess_nav_icon" />
-          <span className="sb_quickAccess_nav_text">Home</span>
-        </div>
-        <div className="sb_quickAccess_nav">
-          <div className="sb_quickAccess_nav_indicator"></div>
-          <ExploreOutlinedIcon className="sb_quickAccess_nav_icon" />
-          <span className="sb_quickAccess_nav_text">Browse</span>
+      <div className="sb_section">
+        <div className="sb_sectionHeader">Quick Access</div>
+        <div className="sb_section_navs">
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <HomeIcon className="sb_section_nav_icon" />
+            <span className="sb_section_nav_text">Home</span>
+          </div>
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <ExploreOutlinedIcon className="sb_section_nav_icon" />
+            <span className="sb_section_nav_text">Browse</span>
+          </div>
         </div>
       </div>
-    </div>
+
+      <hr className="sb_divider" />
+
+      <div className="sb_section">
+        <div className="sb_sectionHeader">Your Library</div>
+        <div className="sb_section_navs">
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <img src={AlbumIcon} alt="albums" className="sb_section_nav_icon" />
+            <span className="sb_section_nav_text">Albums</span>
+          </div>
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <img src={MicIcon} alt="artists" className="sb_section_nav_icon" />
+            <span className="sb_section_nav_text">Artists</span>
+          </div>
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <img
+              src={AlbumIcon}
+              alt="local files"
+              className="sb_section_nav_icon"
+            />
+            <span className="sb_section_nav_text">Local Files</span>
+          </div>
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <img
+              src={PlaceIcon}
+              alt="recently played"
+              className="sb_section_nav_icon"
+            />
+            <span className="sb_section_nav_text">Recently Played</span>
+          </div>
+          <div className="sb_section_nav">
+            <div className="sb_section_nav_indicator"></div>
+            <img
+              src={PlaylistIcon}
+              alt="playlists"
+              className="sb_section_nav_icon"
+            />
+            <span className="sb_section_nav_text">Playlists</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="sb_addPlaylist">
+        <input
+          type="text"
+          placeholder="New Playlist"
+          className="sb_addPlayList_input"
+        />
+      </div>
+    </PerfectScrollbar>
   </div>
 );
