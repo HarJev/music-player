65d5290d5e21121d11d3b60a428407fc9767086b
Jevaun Harris <jevaun.harris@smsja.net>
2019-09-14 17:57:15 -0500

    Created grid layout

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import { Switch, Route, withRouter } from 'react-router-dom';
 import { LandingPage } from './components/pages/LandingPage';
 import { SideBar } from './components/common/Sidebar';
 
+import './App.css';
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <SideBar />
+        <SideBar className="Right_sidebar" />
         {/* <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/home" component={LandingPage} />
         </Switch> */}
+        <div className="Main">
+          <div className="Main_top">
+            <div className="Main_app"></div>
+            <div className="Left_sidebar"></div>
+          </div>
+          <div className="controls"></div>
+        </div>
       </div>
     );
   }
