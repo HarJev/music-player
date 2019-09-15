34c674c4a27f201a77bcab68601bc8734b6c81d2
Jevaun Harris <jevaun.harris@smsja.net>
2019-09-14 21:47:14 -0500

    Added playback control icons

--- src/components/common/Controls/View.js
+++ src/components/common/Controls/View.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+// import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import LoopIcon from '@material-ui/icons/Loop';
 
 import './style.css';
 
@@ -16,6 +23,28 @@ export const ControlsView = () => (
           <div className="c_textData">
             <div className="c_songName">Stronger</div>
             <div className="c_artist">Kanye West</div>
+          </div>
+          <div className="c_like" style={{ color: '#ffffff' }}>
+            <FavoriteBorderIcon />
+          </div>
+        </div>
+        <div className="c_playbackControls_div">
+          <div className="c_playbackControls">
+            <div className="c_otherIcon">
+              <ShuffleIcon className="c_otherIcon" />
+            </div>
+            <div className="c_otherIcon">
+              <SkipPreviousIcon className="c_otherIcon" />
+            </div>
+            <div className="c_PausePlayIcon">
+              <PauseCircleOutlineIcon className="c_PausePlayIcon" />
+            </div>
+            <div className="c_otherIcon">
+              <SkipNextIcon className="c_otherIcon" />
+            </div>
+            <div className="c_otherIcon">
+              <LoopIcon className="c_otherIcon" />
+            </div>
           </div>
         </div>
       </div>
