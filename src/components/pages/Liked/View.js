a4ec7a5de79b0be3b79667e26b781a8f7ee4b9c0
Jevaun Harris <jevaun.harris@smsja.net>
2019-09-19 22:50:17 -0500

    Created Heading

--- src/components/pages/Liked/View.js
+++ src/components/pages/Liked/View.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import './style.css';
+
 export const Liked = props => (
-  <div>
-    <div />
+  <div className="liked_box">
+    <div className="liked_header">
+      <div className="liked_header_image" />
+      <div className="liked_header_text">
+        <div className="liked_header_title">Songs You Liked</div>
+        <div className="liked_header_subtext">
+          Your heart, hearted these songs!
+        </div>
+        <div className="liked_header_divider" />
+      </div>
+    </div>
   </div>
 );
 
